c977bf542b0992a57f0dd212e0939f85e57d5c21
restvirgcarmen-ux <restvirgcarmen@gmail.com>
2026-08-30 23:41:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const $ = x => document.getElementById(x);
 
-const BACKEND = "https://animador-ia-backend.onrender.com";
+const BACKEND = "https://animador-ia-v2-backend.onrender.com";
 
 const templates = {
   animador: [
